a32c59eb50899fdb31e46bdb797f76ce7aca6551
RawrysMode <71321626+RawrysMode@users.noreply.github.com>
2022-11-08 04:03:54 +0700

    Update Employee.cpp
    added load method

--- odbcapplication/odbcapplication/models/Employee.cpp
+++ odbcapplication/odbcapplication/models/Employee.cpp
@@ -12,6 +12,17 @@ Employee::Employee() {
     this->salary = 0;
 }
 
+Employee::Employee(int id, string firstname, string patronymic, string lastname, string dateOfBirth, string residentialAddress, int jobId, int salary) {
+    this->id = id;
+    this->firstname = firstname;
+    this->patronymic = patronymic;
+    this->lastname = lastname;
+    this->dateOfBirth = dateOfBirth;
+    this->residentialAddress = residentialAddress;
+    this->jobId = jobId;
+    this->salary = salary;
+}
+
 Employee::Employee(int id) {
     this->id = id;
     this->firstname = "";
@@ -109,4 +120,39 @@ bool Employee::remove(HDBC hDBC) {
     }
 
     return true;
+}
+
+Employee Employee::load(HDBC hDBC) {
+    RETCODE retCode;
+    SQLLEN cbValue = SQL_NTS;
+    HSTMT hStmt;
+    const int LEN = 30;
+    SQLCHAR firstname[LEN], patronymic[LEN], lastname[LEN], dateOfBirth[LEN], residentialAddress[50];
+    SQLLEN employeeId = 0, jobId = 0, salary = 0, len = 0;
+
+    SQLCHAR* sql = (SQLCHAR*)"select id, firstname, patronymic, lastname, date_of_birth, residential_address, job_id, salary from employees WHERE id = ?;";
+
+    try {
+        checkRetCodeException(SQLAllocHandle(SQL_HANDLE_STMT, hDBC, &hStmt));
+        checkRetCodeException(SQLPrepareA(hStmt, sql, SQL_NTS), "Prepare SQL");
+        checkRetCodeException(SQLBindParameter(hStmt, 1, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 1, 0, (SQLPOINTER)&id, 0, &cbValue), "Bind parameter");
+        checkRetCodeException(SQLExecute(hStmt), "Execute stmt");
+
+        checkRetCodeException(SQLBindCol(hStmt, 1, SQL_C_LONG, &employeeId, 1, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 2, SQL_C_CHAR, &firstname, LEN, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 3, SQL_C_CHAR, &patronymic, LEN, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 4, SQL_C_CHAR, &lastname, LEN, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 5, SQL_C_CHAR, &dateOfBirth, LEN, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 6, SQL_C_CHAR, &residentialAddress, 50, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 7, SQL_C_LONG, &jobId, 1, &len));
+        checkRetCodeException(SQLBindCol(hStmt, 8, SQL_C_LONG, &salary, 1, &len));
+        checkRetCodeException(SQLFetch(hStmt));
+        return Employee((long long)employeeId, string((char*)firstname), string((char*)patronymic), string((char*)lastname), string((char*)dateOfBirth), string((char*)residentialAddress), (long long)jobId, (long long)salary);
+        SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
+    }
+    catch (ActiveRecordException& exc) {
+        cout << "Load clients failed: " << exc.retCode << " " << exc.message << endl;
+    }
+
+    return Employee();
 }
